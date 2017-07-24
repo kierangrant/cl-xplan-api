2724892313cceb61de6697b1f033139a30d9eacc
Kieran Grant <kieran.thehacker.grant@gmail.com>
2017-07-24 19:11:18 +1000

    Minor cleanups in styling

--- src/api/assumption_set.lisp
+++ src/api/assumption_set.lisp
@@ -17,10 +17,8 @@ Description: /assumption_set API Functions
 
 (in-package :cl-xplan-api/api)
 
-;;; assumption_set
-
 ;; assumption_set - GET /resourceful/assumption_set and GET /resourceful/assumption_set/:assumption_set_name
 
-(define-entrypoint assumption_set :get (assumption_set_name) ()
-		   :resource (format NIL "/assumption_set~@[/~A~]" assumption_set_name))
-
+(define-entrypoint assumption_set :get
+  (assumption_set_name) ()
+  :resource (format NIL "/assumption_set~@[/~A~]" assumption_set_name))
