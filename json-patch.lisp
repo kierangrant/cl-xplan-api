f7a85a0c7a03160179c532a8cac2d00f3e5f2619
Kieran Grant <kieran.thehacker.grant@gmail.com>
2017-08-03 15:08:53 +1000

    Small fixup for json-patch.lisp to make it work correctly

--- json-patch.lisp
+++ json-patch.lisp
@@ -1,8 +1,8 @@
 ;;; Only try and define +JSON-FALSE+ if it is not in CL-JSON already.
+(in-package :json)
 (eval-when (:compile-toplevel :load-toplevel :execute)
-  (if (not (find-symbol "+JSON-FALSE+" (find-package "CL-JSON")))
+  (if (not (eq (elt (multiple-value-list (find-symbol "+JSON-FALSE+")) 1) :external))
       (progn
-	(in-package :json)
 	(defconstant +JSON-FALSE+ '+JSON-FALSE+ "Special Constant to force the output of 'false' in an encoded JSON structure. Some applications are known to test for a value of 'false', meaning that a value of 'null' is not enough, this value can be used for those special occasions.")
 	(defparameter +json-lisp-symbol-tokens+
 	  '(("true" . t)
