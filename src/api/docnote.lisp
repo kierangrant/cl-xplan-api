kierangrant/cl-xplan-api

commit 85d777526dc92f28e3c3dcdc67d4e4ed8eb8c97c

Partial reverse of a891c90f4877156b26c4419c18ad10042cb20380
Commit a891c90f4877156b26c4419c18ad10042cb20380 introduced changes
to certain API calls that had objects as parameters.
We reverse this change, and allow those fields to be manually specified.
But we keep backward compatability by allowing user to manually provde
a-list, p-list or hash-table of these objects.

src/api/docnote.lisp
@@ -21,22 +21,53 @@ Description: /docnote API Functions
 
 ;; docnote - GET /resourceful/docnote and GET /resourceful/docnote/:docid
 (define-entrypoint docnote :get
-  (docid)
+  (docid created_date.start_date created_date.end_date modified_date.start_date
+	 modified_date.end_date reference_date.start_date reference_date.end_date
+	 firstread.entity_id (firstread.include_read_notes nil read-notes-p)
+	 firstread.read_entity_roles firstread.max_num_entities)
   ((entity_ids :cond (and (not docid) entity_ids))
    (subject :cond (and (not docid) subject))
-   (created_by :cond (and (not docid) created_by))
+   ((created_by nil created_by-p) :cond (not docid)
+    :value
+    (if created_by-p created_by
+	(let ((h (make-hash-table)))
+	  (if created_date.start_date (setf (gethash "start_date" h) created_date.start_date))
+	  (if created_date.end_date (setf (gethash "end_date" h) created_date.end_date))
+	  h)))
    ((has_metadata nil metadata-p) :cond (and (not docid) metadata-p) :value (if has_metadata 1 0))
    (categories :cond (and (not docid) categories))
    (link_thread :cond (and (not docid) link_thread))
    (doctype :cond (and (not docid) doctype))
    (docsubtype :cond (and (not docid) docsubtype))
-   (modified_by :cond (and (not docid) modified_by))
+   ((modified_by nil modified_by-p) :cond (not docid)
+    :value
+    (if modified_by-p modified_by
+	(let ((h (make-hash-table)))
+	  (if modified_date.start_date (setf (gethash "start_date" h) modified_date.start_date))
+	  (if modified_date.end_date (setf (gethash "end_date" h) modified_date.end_date))
+	  h)))
    (keywords :cond (and (not docid) keywords))
    (created_date :cond (and (not docid) created_date))
    (modified_date :cond (and (not docid) modified_date))
-   (reference_date :cond (and (not docid) reference_date))
+   ((reference_date nil reference_date-p) :cond (not docid)
+    :value
+    (if reference_date-p reference_date
+	(let ((h (make-hash-table)))
+	  (if reference_date.start_date (setf (gethash "start_date" h) reference_date.start_date))
+	  (if reference_date.end_date (setf (gethash "end_date" h) reference_date.start_date))
+	  h)))
    ((session_info nil sinfo-p) :cond (and (not docid) sinfo-p) :value (if session_info 1 0))
-   (firstread :cond (and (not docid) firstread))
+   ((firstread nil firstread-p) :cond (not docid)
+    :value
+    (if firstread-p firstread
+	(let ((h (make-hash-table)))
+	  (if firstread.entity_id (setf (gethash "entity_id" h) firstread.entity_id))
+	  (if read-notes-p (setf (gethash "include_read_notes" h) firstread.include_read_notes))
+	  (if firstread.read_entity_roles
+	      (setf (gethash "read_entity_roles" h) firstread.read_entity_roles))
+	  (if firstread.max_num_entities
+	      (setf (gethash "max_num_entities" h) firstread.max_num_entities))
+	  h)))
    (page_size :cond (and (not docid) page_size))
    (page_sort :cond (and (not docid) page_sort))
    (page_bookmark :cond (and (not docid) page_bookmark))
@@ -108,7 +139,10 @@ Description: /docnote API Functions
 
 ;; docnote-v2 - GET /resourceful/docnote-v2 and GET /resourceful/docnote-v2/:docid
 (define-entrypoint docnote-v2 :get
-  (docid)
+  (docid created_date.start_date created_date.end_date modified_date.start_date modified_date.end_date
+	 reference_date.start_date reference_date.end_date firstread.entity_id
+	 (firstread.include_read_notes nil include-read-p) firstread.read_entity_roles
+	 firstread.max_num_entities)
   ((entity_ids :cond (and (not docid) entity_ids))
    (subject :cond (and (not docid) subject))
    (created_by :cond (and (not docid) created_by))
@@ -119,16 +153,40 @@ Description: /docnote API Functions
    (docsubtype :cond (and (not docid) docsubtype))
    (modified_by :cond (and (not docid) modified_by))
    (keywords :cond (and (not docid) keywords))
-   (created_date.start_date :cond (and (not docid) created_date.start_date))
-   (created_date.end_date :cond (and (not docid) created_date.end_date))
-   (modified_date.start_date :cond (and (not docid) modified_date.start_date))
-   (modified_date.end_date :cond (and (not docid) modified_date.end_date))
-   (reference_date.start_date :cond (and (not docid) reference_date.start_date))
-   (reference_date.end_date :cond (and (not docid) reference_date.end_date))
+   ((created_date nil created_date-p) :cond (not docid)
+    :value
+    (if created_date-p created_date
+	(let ((h (make-hash-table)))
+	  (if created_date.start_date (setf (gethash "start_date" h) created_date.start_date))
+	  (if created_date.end_date (setf (gethash "end_date" h) created_date.end_date))
+	  h)))
+   ((modified_date nil modified_date-p) :cond (not docid)
+    :value
+    (if modified_date-p modified_date
+	(let ((h (make-hash-table)))
+	  (if modified_date.start_date (setf (gethash "start_date" h) modified_date.start_date))
+	  (if modified_date.end_date (setf (gethash "end_date" h) modified_date.end_date))
+	  h)))
+   ((reference_date nil reference_date-p) :cond (not docid)
+    :value
+    (if reference_date-p reference_date
+	(let ((h (make-hash-table)))
+	  (if reference_date.start_date (setf (gethash "start_date" h) reference_date.start_date))
+	  (if reference_date.end_date (setf (gethash "end_date" h) reference_date.end_date))
+	  h)))
    ((session_info nil sinfo-p) :cond (and (not docid) sinfo-p) :value (if session_info 1 0))
-   (firstread.entity_id :cond (and (not docid) firstread.entity_id))
-   ((firstread.include_read_notes nil include-read-p) :cond (and (not include-read-p))
-    :value (if firstread.include_read_notes 1 0))
+   ((firstread nil firstread-p) :cond (not docid)
+    :value
+    (if firstread-p firstread
+	(let ((h (make-hash-table)))
+	  (if firstread.entity_id (setf (gethash "entity_id" h) firstread.entity_id))
+	  (if include-read-p (setf (gethash "include_read_notes" h)
+				   (if firstread.include_read_notes 1 0)))
+	  (if firstread.read_entity_roles
+	      (setf (gethash "read_entity_roles" h) firstread.read_entity_roles))
+	  (if firstread.max_num_entities
+	      (setf (gethash "max_num_entities" h) firstread.max_num_entities))
+	  h)))
    (page_size :cond (and (not docid) page_size))
    (page_sort :cond (and (not docid) page_sort))
    (page_bookmark :cond (and (not docid) page_bookmark))

src/api/case_manager.lisp
@@ -34,12 +34,91 @@ Description: /case_manager API Functions
 
 ;; case_manager - POST /resourceful/case_manager
 
-(define-entrypoint case_manager :post () (fields) :resource "/case_manager" :single-parms-as-body T)
+(define-entrypoint case_manager :post
+  (status createdstamp date_due description creator fua assignee manual_override_productname locked
+	  forecast_fua navigation_flag instigation_type forecast_ongoing category likelihood name
+	  advice_container forecast_revenue revenue external_id client ongoing template
+	  manual_override_premium scope from_template (is_joint nil joint-p))
+  (((fields nil fields-p) :cond T ;; always include, just matter of whether we build it...
+    :value
+    (if fields-p fields
+	(let ((h (make-hash-table)))
+	  (if status (setf (gethash "status" h) status))
+	  (if createdstamp (setf (gethash "createdstamp" h) createdstamp))
+	  (if date_due (setf (gethash "date_due" h) date_due))
+	  (if description (setf (gethash "description" h) description))
+	  (if creator (setf (gethash "creator" h) creator))
+	  (if fua (setf (gethash "fua" h) fua))
+	  (if assignee (setf (gethash "assignee" h) assignee))
+	  (if manual_override_productname
+	      (setf (gethash "manual_override_productname" h) manual_override_productname))
+	  (if locked (setf (gethash "locked" h) locked))
+	  (if forecast_fua (setf (gethash "forecast_fua" h) forecast_fua))
+	  (if navigation_flag (setf (gethash "navigation_flag" h) navigation_flag))
+	  (if instigation_type (setf (gethash "instigation_type" h) instigation_type))
+	  (if forecast_ongoing (setf (gethash "forecast_ongoing" h) forecast_ongoing))
+	  (if category (setf (gethash "category" h) category))
+	  (if likelihood (setf (gethash "likelihood" h) likelihood))
+	  (if name (setf (gethash "name" h) name))
+	  (if advice_container (setf (gethash "advice_container" h) advice_container))
+	  (if forecast_revenue (setf (gethash "forecast_revenue" h) forecast_revenue))
+	  (if revenue (setf (gethash "revenue" h) revenue))
+	  (if external_id (setf (gethash "external_id" h) external_id))
+	  (if client (setf (gethash "client" h) client))
+	  (if ongoing (setf (gethash "ongoing" h) ongoing))
+	  (if template (setf (gethash "template" h) template))
+	  (if manual_override_premium (setf (gethash "manual_override_premium" h) manual_override_premium))
+	  (if scope (setf (gethash "scope" h) scope))
+	  (if from_template (setf (gethash "from_template" h) from_template))
+	  (if joint-p (setf (gethash "is_joint" h) (if is_joint 1 0)))
+	  h))))
+  :resource "/case_manager"
+  :single-parms-as-body T)
 
 ;; case_manager - PATCH /resourceful/case_manager/:container_id
 
-(define-entrypoint case_manager :patch (container_id) (fields) :single-parms-as-body T
-		   :resource (format NIL "/case_manager/~A" container_id))
+(define-entrypoint case_manager :patch
+
+  (container_id status createdstamp date_due description creator fua assignee
+		manual_override_productname locked forecast_fua navigation_flag instigation_type
+		forecast_ongoing category likelihood name advice_container forecast_revenue revenue
+		external_id client ongoing template manual_override_premium scope from_template
+		(is_joint nil joint-p))
+  (((fields nil fields-p) :cond T ;; always include, just matter of whether we build it...
+    :value
+    (if fields-p fields
+	(let ((h (make-hash-table)))
+	  (if status (setf (gethash "status" h) status))
+	  (if createdstamp (setf (gethash "createdstamp" h) createdstamp))
+	  (if date_due (setf (gethash "date_due" h) date_due))
+	  (if description (setf (gethash "description" h) description))
+	  (if creator (setf (gethash "creator" h) creator))
+	  (if fua (setf (gethash "fua" h) fua))
+	  (if assignee (setf (gethash "assignee" h) assignee))
+	  (if manual_override_productname
+	      (setf (gethash "manual_override_productname" h) manual_override_productname))
+	  (if locked (setf (gethash "locked" h) locked))
+	  (if forecast_fua (setf (gethash "forecast_fua" h) forecast_fua))
+	  (if navigation_flag (setf (gethash "navigation_flag" h) navigation_flag))
+	  (if instigation_type (setf (gethash "instigation_type" h) instigation_type))
+	  (if forecast_ongoing (setf (gethash "forecast_ongoing" h) forecast_ongoing))
+	  (if category (setf (gethash "category" h) category))
+	  (if likelihood (setf (gethash "likelihood" h) likelihood))
+	  (if name (setf (gethash "name" h) name))
+	  (if advice_container (setf (gethash "advice_container" h) advice_container))
+	  (if forecast_revenue (setf (gethash "forecast_revenue" h) forecast_revenue))
+	  (if revenue (setf (gethash "revenue" h) revenue))
+	  (if external_id (setf (gethash "external_id" h) external_id))
+	  (if client (setf (gethash "client" h) client))
+	  (if ongoing (setf (gethash "ongoing" h) ongoing))
+	  (if template (setf (gethash "template" h) template))
+	  (if manual_override_premium (setf (gethash "manual_override_premium" h) manual_override_premium))
+	  (if scope (setf (gethash "scope" h) scope))
+	  (if from_template (setf (gethash "from_template" h) from_template))
+	  (if joint-p (setf (gethash "is_joint" h) (if is_joint 1 0)))
+	  h))))
+  :single-parms-as-body T
+  :resource (format NIL "/case_manager/~A" container_id))
 
 ;; case_manager - DELETE /resourceful/case_manager/:container_id
 
@@ -56,5 +135,43 @@ Description: /case_manager API Functions
 
 ;; case_manager-v2 - POST /resourceful/case_manager-v2
 
-(define-entrypoint case_manager-v2 :post () (fields) :resource "/case_manager-v2"
-		   :single-parms-as-body T)
+(define-entrypoint case_manager-v2 :post
+  (status createdstamp date_due description creator fua assignee manual_override_productname locked
+	  forecast_fua navigation_flag instigation_type forecast_ongoing category likelihood name
+	  advice_container forecast_revenue revenue external_id client ongoing template
+	  manual_override_premium scope from_template (is_joint nil joint-p))
+  (((fields nil fields-p) :cond T ;; always include, just matter of whether we build it...
+    :value
+    (if fields-p fields
+	(let ((h (make-hash-table)))
+	  (if status (setf (gethash "status" h) status))
+	  (if createdstamp (setf (gethash "createdstamp" h) createdstamp))
+	  (if date_due (setf (gethash "date_due" h) date_due))
+	  (if description (setf (gethash "description" h) description))
+	  (if creator (setf (gethash "creator" h) creator))
+	  (if fua (setf (gethash "fua" h) fua))
+	  (if assignee (setf (gethash "assignee" h) assignee))
+	  (if manual_override_productname
+	      (setf (gethash "manual_override_productname" h) manual_override_productname))
+	  (if locked (setf (gethash "locked" h) locked))
+	  (if forecast_fua (setf (gethash "forecast_fua" h) forecast_fua))
+	  (if navigation_flag (setf (gethash "navigation_flag" h) navigation_flag))
+	  (if instigation_type (setf (gethash "instigation_type" h) instigation_type))
+	  (if forecast_ongoing (setf (gethash "forecast_ongoing" h) forecast_ongoing))
+	  (if category (setf (gethash "category" h) category))
+	  (if likelihood (setf (gethash "likelihood" h) likelihood))
+	  (if name (setf (gethash "name" h) name))
+	  (if advice_container (setf (gethash "advice_container" h) advice_container))
+	  (if forecast_revenue (setf (gethash "forecast_revenue" h) forecast_revenue))
+	  (if revenue (setf (gethash "revenue" h) revenue))
+	  (if external_id (setf (gethash "external_id" h) external_id))
+	  (if client (setf (gethash "client" h) client))
+	  (if ongoing (setf (gethash "ongoing" h) ongoing))
+	  (if template (setf (gethash "template" h) template))
+	  (if manual_override_premium (setf (gethash "manual_override_premium" h) manual_override_premium))
+	  (if scope (setf (gethash "scope" h) scope))
+	  (if from_template (setf (gethash "from_template" h) from_template))
+	  (if joint-p (setf (gethash "is_joint" h) (if is_joint 1 0)))
+	  h))))
+  :resource "/case_manager-v2"
+  :single-parms-as-body T)

src/api/disclaimer/orders.lisp
@@ -26,6 +26,7 @@ Description: /disclaimer/orders API Functions
   ()
   (((activated nil activated-p) :cond activated-p :value (if activated 1 0))
    content button display_mode)
+  :single-parms-as-body T
   :resource "/disclaimer/orders")
 
 ;; disclaimer/orders - POST /resourceful/disclaimer/orders?_method=accept

commit 561172d68a5b66c37d2084bec698c27b57ad4ed7

API: Fix error in docnote date handling
In DocNote (V1) there was a rather obvious brain-fart in the code,
namely, created_by/created_date got mangled.
Bug introduced in commit 85d777526dc92f28e3c3dcdc67d4e4ed8eb8c97c

src/api/docnote.lisp
@@ -27,26 +27,26 @@ Description: /docnote API Functions
 	 firstread.read_entity_roles firstread.max_num_entities)
   ((entity_ids :cond (and (not docid) entity_ids))
    (subject :cond (and (not docid) subject))
-   ((created_by nil created_by-p) :cond (not docid)
-    :value
-    (if created_by-p created_by
-	(cond-hash
-	  (created_date.start_date "start_date")
-	  (created_date.end_date "end_date"))))
+   (created_by :cond (and (not docid) created_by))
    ((has_metadata nil metadata-p) :cond (and (not docid) metadata-p) :value (if has_metadata 1 0))
    (categories :cond (and (not docid) categories))
    (link_thread :cond (and (not docid) link_thread))
    (doctype :cond (and (not docid) doctype))
    (docsubtype :cond (and (not docid) docsubtype))
-   ((modified_by nil modified_by-p) :cond (not docid)
+   (modified_by :cond (and (not docid) modified_by))
+   (keywords :cond (and (not docid) keywords))
+   ((created_date nil created_date-p) :cond (not docid)
     :value
-    (if modified_by-p modified_by
+    (if created_date-p created_date
+	(cond-hash
+	  (created_date.start_date "start_date")
+	  (created_date.end_date "end_date"))))
+   ((modified_date nil modified_date-p) :cond (not docid)
+    :value
+    (if modified_date-p modified_date
 	(cond-hash
 	  (modified_date.start_date "start_date")
 	  (modified_date.end_date "end_date"))))
-   (keywords :cond (and (not docid) keywords))
-   (created_date :cond (and (not docid) created_date))
-   (modified_date :cond (and (not docid) modified_date))
    ((reference_date nil reference_date-p) :cond (not docid)
     :value
     (if reference_date-p reference_date
@@ -174,6 +174,10 @@ Description: /docnote API Functions
 	  (include-read-p "include_read_notes" (if firstread.include_read_notes 1 0))
 	  (firstread.read_entity_roles "read_entity_roles")
 	  (firstread.max_num_entities "max_num_entities"))))
+   (doctype_exclude :cond (and (not docid) doctype_exclude))
+   (docsubtype_exclude :cond (and (not docid) docsubtype_exclude))
+   ((has_digital_signature nil has_digital_signature-p) :cond (and (not docid) has_digital_signature-p)
+    :value (if has_digital_signature 1 0))
    (page_size :cond (and (not docid) page_size))
    (page_sort :cond (and (not docid) page_sort))
    (page_bookmark :cond (and (not docid) page_bookmark))