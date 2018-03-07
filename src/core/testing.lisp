e12302a67a7526c436b15644231b35b71723be63
Kieran Grant <kieran.thehacker.grant@gmail.com>
2018-03-08 09:22:56 +1000

    Add testing session
    New Testing Session is for testing and development of this library.
    Whilst you can use an XPLANID cookie from a XPLAN session for testing
    purposes, this interface will not be made public, so it is subject
    to change and removal at any time.
    
    For production, you MUST use an API key.

---
+++ src/core/testing.lisp
@@ -0,0 +1,59 @@
+#|
+This file is part of CL-XPLAN-API, the Lisp XPLAN API Library
+
+Copyright (C) 2018 Kieran Grant
+This library is free software; you can redistribute it and/or
+modify it under the terms of the Lisp Lesser General Public License
+(http://opensource.franz.com/preamble.html), known as the LLGPL.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+Lisp Lesser GNU General Public License for more details.
+
+File: src/core/testing.lisp
+Description: Testing Session for testing and development purposes.
+|#
+
+(in-package :cl-xplan-api/core)
+
+;;; This file is for TESTING AND DEVELOPMENT ONLY
+;;; It is not supported for actual usage
+
+(defclass xplan-session-test (xplan-session)
+  ((xplanid :initarg :xplanid :initform "")
+   (trace :initarg :trace :initform nil)))
+
+(defmethod %process-request ((session xplan-session-test) request &key do-auth)
+  (if do-auth (error "Cannot authenticate with an XPLAN-SESSION-TEST session!"))
+  (with-slots (drakma-settings) session
+    (let ((cookies
+	   (make-instance 'drakma:cookie-jar
+			  :cookies `(,(make-instance
+				       'drakma:cookie
+				       :name "XPLANID"
+				       :value (slot-value session 'xplanid)
+				       :securep nil :http-only-p nil
+				       :domain
+				       (subseq (elt
+						(split-sequence:split-sequence
+						 #\: (base-url session))
+						1)
+					       2)))))
+	  (content (get-request-content request))
+	  (content-type (get-request-content-type request)))
+      (if (slot-value session 'trace) (format *debug-io* "~S~%" (drakma:cookie-jar-cookies cookies)))
+    (apply
+     (lambda (&rest rest)
+       (if (slot-value session 'trace)
+	   (format *debug-io* "~S~%" rest))
+       (apply *api-call-function* rest))
+     (get-request-url request)
+     :method (get-request-method request)
+     :force-binary T
+     :cookie-jar cookies
+     :additional-headers '(("Accept" . "application/json"))
+     :user-agent *user-agent*
+     (if (and content content-type)
+	 (append `(:content ,content :content-type ,content-type) drakma-settings)
+	 drakma-settings)))))
